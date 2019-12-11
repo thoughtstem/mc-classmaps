e4e2df618c201979feec774e01eb2e462de589f7
srfoster <stephen@thoughtstem.com>
2019-12-11 12:04:34 -0800

    travis

--- info.rkt
+++ info.rkt
@@ -3,6 +3,7 @@
 (define deps '("base"))
 (define build-deps 
   '("scribble-lib" "racket-doc" "rackunit-lib"
+    "https://github.com/thoughtstem/website.git"
     "https://github.com/thoughtstem/website-js.git"))
 (define scribblings '(("scribblings/mc-classmaps.scrbl" ())))
 (define pkg-desc "Description Here")

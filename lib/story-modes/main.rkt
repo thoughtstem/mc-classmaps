b1e5f0544b3c842ee9f8607ae6082ec2e33f9813
Sara <sara@thoughtstem.com>
2019-12-10 16:44:21 -0800

    updated story-mode struct, added helper functions

--- lib/story-modes/main.rkt
+++ lib/story-modes/main.rkt
@@ -28,120 +28,115 @@
 
 (define (coa-stories)
   (story-mode "Code of Awesomeness Fables" 15
-              (div
-               (p "Read the following stories and discuss:")
-               (mode->content-card 
-                #:fade? #f
-                (overcome-the-unknown-fable))
+              "Introduce the Code of Awesomeness Codes with these fables."
+              (story-text "Read the following stories and discuss:"
+                          (mode->content-card 
+                           #:fade? #f
+                           (overcome-the-unknown-fable))
       
-               (mode->content-card 
-                #:fade? #f
-                (bug-hunter-fable))
+                          (mode->content-card 
+                           #:fade? #f
+                           (bug-hunter-fable))
       
-               (mode->content-card 
-                #:fade? #f
-                (energize-the-team-fable)))))
+                          (mode->content-card 
+                           #:fade? #f
+                           (energize-the-team-fable)))))
 
 ; individual CoA fables
 
 (define (overcome-the-unknown-fable)
   (story-mode "Overcome the Unknown Fable" 5
-              (div
-               (p "Once upon a time...")
-               (ipsum 50))))
+              "A Code of Awesomeness fable."
+              (story-text "Once upon a time...")))
 
 
 (define (bug-hunter-fable)
   (story-mode "Be a Bug Hunter Fable" 5
-              (div
-               (p "Once upon a time...")
-               (ipsum 50))))
+              "A Code of Awesomeness fable."
+              (story-text "Once upon a time...")))
 
 (define (energize-the-team-fable)
   (story-mode "Energize the Team Fable" 5
-              (div
-               (p "Once upon a time...")
-               (ipsum 50))))
+              "A Code of Awesomeness fable."
+              (story-text "Once upon a time...")))
 
-;--
 
 
 (define (coach-story)
   (story-mode "Coach Story" 5
-              (div
-               (p "Tell your coach story")
-               (ipsum 50))))
+              "A chance to introduce yourself and some values of MetaCoders."
+              (story-text "Tell your coach story..." )))
 
 (define (class-intro)
   (story-mode "Class Introduction Story" 3
-              (div
-               (p "Introduce the language you will be learning in class.")
-               (p "Introduce the gamification elements you will be using in class (badges, dollars, market, etc)."))))
+              "Set up the goals and norms for the course."
+              (story-text
+               "Introduce the language you will be learning in class."
+               "Introduce the gamification elements you will be using in class (badges, dollars, market, etc).")))
 
 
 (define (intro-jam)
   (story-mode "Final Jam Rules & Resources" 10
-              (div
-               (p "Coach introduces rule, resources, and sets students up for success in their final jam..."))))
+              "Prepare to JAM OUT"
+              (story-text "Coach introduces rule, resources, and sets students up for success in their final jam...")))
 
 (define (coa-focus)
   (story-mode "Code of Awesomeness Focus" 5
-              (div
-               (p "Review all Code of Awesomeness codes and mottos. Coach or team chooses one in particular to focus on today..."))))
+              "Review all Codes and choose one to focus in on for the day."
+              (story-text "Review all Code of Awesomeness codes and mottos. Coach or team chooses one in particular to focus on today...")))
 
 (define (badge-story)
   (story-mode "Badge Story" 5
-              (div
-               (p "A more in-depth look at what badges represent..."))))
+              "The importance of badges and what they mean, in prep for awarding the first badge(s)!"
+              (story-text "A more in-depth look at what badges represent...")))
 
 
 ;======== META STORIES/REFLECTIONS =========
 
 (define (meta-story-one-word)
   (story-mode "Meta Story: One Word" 5
-              (div
-               (p "Each student shares one word about their experience in class that day..."))))
+              "A speedy mini-reflection where each student sums up their experience in one word."
+              (story-text "Each student shares one word about their experience in class that day...")))
 
 (define (meta-story-reflect (topic "what the team did and learned today."))
   (story-mode "Meta Story: Reflection" 5
-              (div
-               (p (~a "Briefly reflect on " topic "...")))))
+              "A chance to practice those metacognition skills."
+              (story-text (~a "Briefly reflect on " topic "..."))))
 
 (define (meta-story-deep-reflect (topic "what the team did and learned today."))
   (story-mode "Meta Story: Deep Reflection" 10
-              (div
-               (p "Reflect on " topic ", giving the time and guidance for deeper, more meaningful reflection."))))
+              "A deeper dive into the subjects, challenges, and skill-building that happened in today's class."
+              (story-text "Reflect on " topic ", giving the time and guidance for deeper, more meaningful reflection.")))
 
 ;TODO: functionalize this to add what to say about next week?
 (define (meta-story-next-time)
   (story-mode "Meta Story: Next Time..." 3
-              (div
-               (p "Coach reminds students of something to do/bring/remember about next class..."))))
+              "A chance to remind students of something for upcoming classes."
+              (story-text "Coach reminds students of something to do/bring/remember about next class...")))
 
 (define (meta-story-badges)
   (story-mode "Meta Story: Badges" 5
-              (div
-               (p "Coach awards badges to students who have earned it and reminds others when they will earn theirs..."))))
+              "Badge ceremony!"
+              (story-text "Coach awards badges to students who have earned it and reminds others when they will earn theirs...")))
 
 ;===== HISTORY STORIES ===
 
 (define (hello-world-story)
   (story-mode "Hello World Story" 5
-              (div
-               (p "Tell the story of 'Hello World' and why it is the name of the first kata.")
-               (ipsum 50))))
+              "About the phrase 'Hello World' in the computer science context"
+              (story-text "Tell the story of 'Hello World' and why it is the name of the first kata.")))
 
 (define (first-bug-story)
   (story-mode "The First Computer Bug..." 5
-              (div
-               (p "Story of moth in Mark II -- not Grace Hopper, not the first use of bug in engineering, but still a cool part of computer history.")
-               ;see https://en.wikipedia.org/wiki/Software_bug#History
-               )))
+              "The story of the moth in Mark II"
+              (story-text "Story of moth in Mark II -- not Grace Hopper, not the first use of bug in engineering, but still a cool part of computer history.")
+              ;see https://en.wikipedia.org/wiki/Software_bug#History
+              ))
 
 (define (y2k-bug-story)
   (story-mode "The Y2K Bug" 5
-              (div
-               (p "Story of the most famous bug that never was -- Y2K"))))
+              "Story of the most famous bug that never was -- Y2K"
+              (story-text "Story of the most famous bug that never was -- Y2K")))
 
 
 ;===== METACOGNATIVE STORIES =====
@@ -149,8 +144,8 @@
 
 (define (rethinking-bugs)
   (story-mode "Where the Bug Lives..." 15
-              (div
-               (p "Story and discussion about bugs and where they come from -- not the computers, but our own thinking and understadning (or lack-there-of) of the code."))))
+              "What really causes bugs -- the computer or our OWN BRAIN."
+              (story-text "Story and discussion about bugs and where they come from -- not the computers, but our own thinking and understanding (or lack-there-of) of the code.")))
 
 
 
